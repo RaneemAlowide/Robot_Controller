5dcacf55fd56a9e5db5136da7100addc998b7ab8
Raneem <r.alowide@gmail.com>
2023-07-20 04:32:41 +0300

    Update lastMovement.php

--- Robotic_Controller/lastMovement.php
+++ Robotic_Controller/lastMovement.php
@@ -1,36 +1,30 @@
 <?php
-// Database connection settings
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "robot_movements";
 
-// Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Query to retrieve the last record from the database
+// Retrieving the last recorded in the database
 $query = "select * from movements order by id desc limit 1";
 
-// Execute the query
 $result = $conn->query($query);
 
 // Check if the query was successful
 if ($result) {
-    // Fetch the data
     $row = $result->fetch_assoc();
 
-    // Process the data
     if ($row) {
-        // Access the columns using their names
         $column2 = $row['direction'];
 
         // Display the data
-        echo "Last movement: $column2";
+        echo "Last movement is: $column2";
     } else {
         echo "no movements";
     }
@@ -38,6 +32,5 @@ if ($result) {
     echo "Error executing the query: " . $conn->error;
 }
 
-// Close the connection
 $conn->close();
 ?>
